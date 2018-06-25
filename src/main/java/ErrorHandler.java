4f11f0cf5030a9e9f295d0a8894d02c450e41dbf
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-06-25 00:48:22 -0500

    Refactored Into ErrorHandler

---
+++ src/main/java/ErrorHandler.java
@@ -0,0 +1,41 @@
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class ErrorHandler {
+
+    private final Stream<String> inputs;
+
+    ErrorHandler(Stream<String> inputs) {
+        this.inputs = inputs;
+    }
+
+    boolean isValidInput() throws NumberFormatException {
+
+        List<Integer> negativeNumbersList = getNegativeNumbers();
+
+        if (negativeNumbersList.size() > 0) {
+            StringBuilder negativeNumbersString = buildNegativeNumberString(negativeNumbersList);
+            throw new InvalidParameterException("Negatives Not allowed ->" + negativeNumbersString);
+        }
+        return true;
+    }
+
+    private StringBuilder buildNegativeNumberString(List<Integer> negativeNumbersList) {
+        StringBuilder negativeNumbersString = new StringBuilder();
+        negativeNumbersList.forEach(negativeNumber ->
+                negativeNumbersString
+                        .append(" ")
+                        .append(negativeNumber));
+        return negativeNumbersString;
+    }
+
+    private List<Integer> getNegativeNumbers() {
+        List<Integer> negativeNumbersList = new ArrayList<>();
+        inputs.mapToInt(Integer::parseInt)
+                .filter(num -> num < 0)
+                .forEach(negativeNumbersList::add);
+        return negativeNumbersList;
+    }
+}
--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,19 +1,15 @@
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 class StringCalculator {
 
     int add(String allInputs) {
-
         InputCreator inputCreator = new InputCreator(allInputs);
-
         Supplier<Stream<String>> supplierInputStream = inputCreator::splitIntoInputs;
 
         try {
-            if (!isValidInput(supplierInputStream.get())) return 0;
+            ErrorHandler errorHandler = new ErrorHandler(supplierInputStream.get());
+            if (!errorHandler.isValidInput()) return 0;
         } catch (NumberFormatException ex) {
             return 0;
         }
@@ -21,33 +17,5 @@ class StringCalculator {
         return supplierInputStream.get()
                 .mapToInt(Integer::parseInt)
                 .sum();
-    }
-
-    private boolean isValidInput(Stream<String> inputs) throws NumberFormatException {
-
-        List<Integer> negativeNumbersList = getNegativeNumbers(inputs);
-
-        if (negativeNumbersList.size() > 0) {
-            StringBuilder negativeNumbersString = buildNegativeNumberString(negativeNumbersList);
-            throw new InvalidParameterException("Negatives Not allowed ->" + negativeNumbersString);
-        }
-        return true;
-    }
-
-    private StringBuilder buildNegativeNumberString(List<Integer> negativeNumbersList) {
-        StringBuilder negativeNumbersString = new StringBuilder();
-        negativeNumbersList.forEach(negativeNumber ->
-                negativeNumbersString
-                        .append(" ")
-                        .append(negativeNumber));
-        return negativeNumbersString;
-    }
-
-    private List<Integer> getNegativeNumbers(Stream<String> inputs) {
-        List<Integer> negativeNumbersList = new ArrayList<>();
-        inputs.mapToInt(Integer::parseInt)
-                .filter(num -> num < 0)
-                .forEach(negativeNumbersList::add);
-        return negativeNumbersList;
     }
 }
