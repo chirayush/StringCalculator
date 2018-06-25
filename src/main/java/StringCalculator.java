2f5a91f4224de70314e8be2204d69c22ef7daf29
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-06-25 13:16:38 -0500

    Implemented during Lunch until Requirement 5

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class StringCalculator {
 
+    int add(String input) {
+        if ("".equals(input)) {
+            return 0;
+        }
+
+        String[] inputs;
+
+        if (input.startsWith("//") && "\n".equals(input.substring(3, 4))) {
+            String customDelimiter = String.valueOf(input.charAt(2));
+            input = input.replace("//" + customDelimiter + "\n", "");
+            inputs = input
+                    .replace(customDelimiter, ",")
+                    .split(",");
+        } else {
+            inputs = input
+                    .replace("\n", ",")
+                    .split(",");
+        }
+
+        List<Integer> negativeNumbers = new ArrayList<>();
+
+        Arrays.stream(inputs)
+                .mapToInt(Integer::parseInt)
+                .filter(num -> num < 0)
+                .collect(negativeNumbers::add);
+
+        return Arrays.stream(inputs)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,65 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    public void addShouldReturn0ForEmptyString() {
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void addShouldReturn3When3IsEntered() {
+        assertEquals(3, stringCalculator.add("3"));
+    }
+
+    @Test
+    public void addShouldReturn5When5IsEntered() {
+        assertEquals(5, stringCalculator.add("5"));
+    }
+
+    @Test
+    public void addShouldReturn8With3And5() {
+        assertEquals(8, stringCalculator.add("3,5"));
+    }
+
+    @Test
+    public void addShouldReturn11With4And7() {
+        assertEquals(11, stringCalculator.add("4,7"));
+    }
+
+    @Test
+    public void addShouldReturn13When3And5And5() {
+        assertEquals(13, stringCalculator.add("3,5,5"));
+    }
+
+    @Test
+    public void addShouldReturnSumWhenNewLineAndCommaDelimiter() {
+        assertEquals(12, stringCalculator.add("3,3\n6"));
+    }
+
+    @Test
+    public void addShouldReturnSumWithCustomDelimiterOfSemiColon() {
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void addShouldReturnSumWithCustomDelimiterOfStar() {
+        assertEquals(7, stringCalculator.add("//*\n2*5"));
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void addShouldThrowExceptionIfNegativeNumber() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("negatives not allowed -3");
+        stringCalculator.add("-3,5");
+    }
 }
