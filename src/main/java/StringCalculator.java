c24cbed1928620858d87ef72e8e2d4586c655b7c
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-06-26 13:43:15 -0500

    Implemented using Scanner

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 class StringCalculator {
 
+    int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        String customDelimiter = " ";
+        if (input.startsWith("//") && "\n".equals(input.substring(3, 4))) {
+            customDelimiter = String.valueOf(input.charAt(2));
+            String initialPattern = "//" + customDelimiter + "\n";
+            input = input.replace(initialPattern, "");
+        }
+        Scanner scanner = new Scanner(input);
+
+        if(customDelimiter.equals(" ")) {
+            scanner.useDelimiter(",|\\n");
+        } else {
+            scanner.useDelimiter(customDelimiter);
+        }
+
+        return sum(scanner);
+
+    }
+
+    private int sum(Scanner scanner) {
+        int sum = 0;
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
+        }
+
+        return sum;
+    }
 }
